23c9885d0f53b8fcb8de465ac6b67d57f19b48a0
Michael <michael.taylor@celebrationsoftware.com>
2020-05-10 14:10:09 -0400

    removed find sound, and give it direct reference for performance

--- Assets/Scripts/AsteroidExplosion.cs
+++ Assets/Scripts/AsteroidExplosion.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class AsteroidExplosion : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("ExplosionSound").GetComponent<AudioSource>().Play();
         Destroy(this.gameObject, 3.0f);
     }
 
